500f0b76f1493ae843f13e9990d1e6b578368475
MentalSkin <tomfneves+github@gmail.com>
2020-03-21 12:03:50 +0000

    render function is working

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 let myLibrary = [];
 
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'No')
+const book2 = new Book('Meditations', 'Marcus Aurelius', 200, 'Yes')
+const book3 = new Book('Thinking Fast and Slow', 'Daniel Kahneman', 400, 'No')
+
+//renders the content from myLibrary into a HTML table
+function render(library){
+    
+    let table = document.getElementById('tableBody')
+    
+    //1. clear table content
+    while (table.rows.length >= 1) {
+        table.deleteRow(0)
+    }
+    //2. Append each book
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        //add new row
+        let row = table.insertRow(0)
+        let titleCell = row.insertCell(0)
+        let authorCell = row.insertCell(1)
+        let pagesCell = row.insertCell(2)
+        let readCell = row.insertCell(3)
+
+        titleCell.innerHTML = library[i].title
+        authorCell.innerHTML = library[i].author
+        pagesCell.innerHTML = library[i].pages
+        readCell.innerHTML = library[i].read
+    }
+}
+
 //Object constructor for Books
 function Book(title, author, pages, read) {
     this.title = title;
@@ -8,22 +38,34 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.info = function() {
         return `${title} by ${author}, ${pages} pages, ${read}`
-        //"The Hobbit by J.R.R. Tolkien, 295 pages, not read yet."
     }
 }
 
-//open form to input text and create new book instance
-function createBook() {
-
+//add a new book to myLibrary; assign to button after user input
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+    render(myLibrary)
 }
 
-//add a new book to myLibrary
-function addBookToLibrary() {
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
 
-}
+
+
+
+
+
+
+
+
+
+
 
 //remove book from myLibrary
 function removeBookFromLibrary() {
 
 }
+
+//toggle read / not read
 
